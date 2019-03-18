59f4ec073284111d1877f4e0d3172a63b381029b
Andrée Hansson <peolanha@gmail.com>
2019-03-18 20:06:24 +0100

    Parallelize menu fetching

--- lunches.js
+++ lunches.js
@@ -9,7 +9,7 @@ const places = [
 ];
 
 const getMenusForDay = (menus, date) => {
-  return menus.map(p => ({ ...p, menu: p.menu[date.getUTCDay() - 1] }));
+  return menus.filter(p => p).map(p => ({ ...p, menu: p.menu[date.getUTCDay() - 1] }));
 };
 
 const formatOne = (item, reaction) => `
@@ -21,20 +21,20 @@ const format = (menus, reactions) => menus.map((m, i) => formatOne(m, reactions[
 
 module.exports = {
   menus: async (when) => {
-    const menus = [];
-    for (const place of places) {
-        const { name, url, parse, raw } = require(`./places/${place}`);
-        try {
-            console.log('fetching', name);
-            let data = await request({ uri: url, timeout: 5000 });
-            //let data = readFileSync(`test/${place}.html`, 'utf-8');
-            if (!raw) data = parseHTML(data);
-            const menu = await parse(data);
-            menus.push({ name, url, menu });
-        } catch(e) {
-            console.log('failed to fetch', name);
-        }
-    }
+    const menus = await Promise.all(places.map(async (place) => {
+      const { name, url, parse, raw } = require(`./places/${place}`);
+      try {
+        console.log('fetching', name);
+        let data = await request({ uri: url, timeout: 5000 });
+        //let data = readFileSync(`test/${place}.html`, 'utf-8');
+        if (!raw) data = parseHTML(data);
+        const menu = await parse(data);
+        return { name, url, menu };
+      } catch(e) {
+        console.log('failed to fetch', name);
+        return null;
+      }
+    }));
     const filtered = getMenusForDay(menus, when);
     return filtered;
   },
